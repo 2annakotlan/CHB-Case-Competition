d1da916280eb3f11a19f515995fe7f51f3c3e5eb
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 13:54:08 -0500

    Update login_file.py

--- login_file.py
+++ login_file.py
@@ -1,9 +1,4 @@
 import streamlit as st
-
-# Function to initialize the page state if not already set
-def get_initialize_page():
-    if 'page' not in st.session_state:
-        st.session_state.page = 'login_signup_page'  # Default starting page
         
 # Function to handle the create account page
 def get_create_account_page():
