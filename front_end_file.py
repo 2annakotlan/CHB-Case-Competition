5fdb8c9b954ce3ccb9769de62d3b388beab22d0b
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 12:33:51 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -6,17 +6,17 @@ from landing_file import get_admin_landing_page, get_student_landing_page
 import streamlit as st
 
 get_initialize_page()  
-if st.session_state.page == 'login_signup':
+if st.session_state.page == 'login_signup_page':
     get_login_signup_page()  
-elif st.session_state.page == 'create_account':
+elif st.session_state.page == 'create_account_page':
     get_create_account_page()  
-elif st.session_state.page == 'sign_in':
+elif st.session_state.page == 'sign_in_page':
     get_sign_in_page()  
 elif st.session_state.page == 'admin_landing_page':
     get_admin_landing_page()  
 elif st.session_state.page == 'student_landing_page':
     get_student_landing_page()  
-if st.session_state.page == 'friend_swiping':
+if st.session_state.page == 'friend_swiping_page':
     get_friend_swiping_page(friend_swiping_df)
 
 
