4dcb06f67e8a23dd7a6f4a5c3a20d433b3f59436
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 13:57:52 -0500

    Update and rename signin_file.py to sign_in_file.py

---
+++ sign_in_file.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Function to handle the sign-in page
+def get_sign_in_page():
+    st.title("Sign In")
+    st.write("Enter your email and password to sign in")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    if st.button("Sign In"):
+        if email == "admin@falcon.bentley.edu":  # Check if the email is admin
+            st.session_state.page = "admin_landing_page"  # Go to admin landing page
+        else:
+            st.session_state.page = "student_landing_page"  # Go to student landing page
+        st.success("You are signed in successfully!")
