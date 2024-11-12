6b1455d9bf168003ad9d09e7ef539ae0f7dd7729
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 12:49:59 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -1,9 +1,9 @@
 # IMPORT PACKAGES AND OBJECTS 
 from data_file import friend_swiping_df
 from friend_swiping_file import get_friend_swiping_page
-from login_file import get_initialize_page, get_login_signup_page, get_create_account_page, get_signin_page
+from login_file import get_initialize_page, get_login_signup_page, get_create_account_page, get_sign_in_page
 from landing_file import get_admin_landing_page, get_student_landing_page
-import streamlit as s
+import streamlit as st
 
 get_initialize_page()  
 if st.session_state.page == 'login_signup_page':
