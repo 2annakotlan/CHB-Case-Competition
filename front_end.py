3a5ed26f9133b454bce7525c06154eb9ad2f2be0
Anna Kotlan <2annakotlan@gmail.com>
2024-11-11 23:52:37 -0500

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -1,5 +1,7 @@
 # IMPORT PACKAGES AND OBJECTS 
 from data import friend_swiping_df
-from friend_swiping import get_friend_swiping_visual
+from friend_swiping import get_friend_swiping_page
+from login_signup import get_login_signup_page
 
-get_friend_swiping_visual(friend_swiping_df)
+get_login_signup_page("https://github.com/2annakotlan/CHB-Case-Competition/raw/main/homepage_blue.png")
+get_friend_swiping_page(friend_swiping_df)
