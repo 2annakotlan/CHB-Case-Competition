e83afbb39c0251c9ba4d195156f2bafa2c94dd5c
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 02:30:48 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -3,20 +3,3 @@ from data_file import friend_swiping_df
 from friend_swiping_file import get_friend_swiping_page
 from login_signup_file import get_login_signup_page
 
-import streamlit as st
-
-# Custom CSS to set a medium blue background
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #4682B4;  /* Medium Blue */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Your Streamlit app content
-st.title('My Streamlit App with a Medium Blue Background')
-st.write("Welcome to my Streamlit app with a customized background color!")
