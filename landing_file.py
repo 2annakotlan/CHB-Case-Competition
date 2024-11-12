28435722f1a95e7f777d9708ac04996f2075a58a
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 12:18:41 -0500

    Update landing_file.py

--- landing_file.py
+++ landing_file.py
@@ -1,5 +1,5 @@
 # Function to handle the admin landing page view
-def admin_landing_page():
+def get_admin_landing_page():
     st.title("Admin Dashboard")
     st.write("Welcome to the admin panel!")
     st.write("Here you can manage users, monitor activity, etc.")
@@ -9,7 +9,7 @@ def admin_landing_page():
         st.session_state.page = "login_signup"
 
 # Function to handle the student landing page view
-def student_landing_page():
+def get_student_landing_page():
     st.title("Student Dashboard")
     st.write("Welcome, student! This is your personal dashboard.")
     st.write("Explore resources, courses, and more.")
@@ -18,9 +18,3 @@ def student_landing_page():
     if st.button('Log out'):
         st.session_state.page = "login_signup"
 
-# Function to control the flow of the app
-def get_landing_page():
-    if st.session_state.page == 'admin_landing_page':
-        admin_landing_page()  # Show the admin landing page
-    elif st.session_state.page == 'student_landing_page':
-        student_landing_page()  # Show the student landing page
