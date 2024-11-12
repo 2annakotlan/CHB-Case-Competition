78fb6c9b54957bc03c25faa0a57cf33395e6d756
Anna Kotlan <2annakotlan@gmail.com>
2024-11-11 23:20:47 -0500

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -1,3 +1,4 @@
 from data import friend_swiping_df
-#from friend_swiping import 
-get_friend_swiping(sample_data)
+from friend_swiping import get_friend_swiping_visual
+
+get_friend_swiping_visual(friend_swiping_df)
