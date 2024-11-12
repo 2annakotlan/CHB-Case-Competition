bc4dcf54e66bfc06f55c482365826fde2d7f30c4
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 02:10:01 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -7,6 +7,6 @@ from login_signup_file import get_login_signup_page
 # Example usage with two image URLs
 image_url_1 = "https://github.com/2annakotlan/CHB-Case-Competition/raw/main/Better Together 1.png"
 image_url_2 = "https://github.com/2annakotlan/CHB-Case-Competition/raw/main/Better Together 2.png"
-get_login_signup_page(image_url_1, image_url_2)
+get_login_signup_page(image_url_2)
 
 #get_friend_swiping_page(friend_swiping_df)
