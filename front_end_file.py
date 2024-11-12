75538fd8ff28b5a0feae81baf0d8ce87976e3833
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 04:03:53 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -2,6 +2,7 @@
 from data_file import friend_swiping_df
 from friend_swiping_file import get_friend_swiping_page
 from get_started_file import get_started_page
+from landing_file import get_landing_page
 
 get_started_page()
-
+get_landing_page()
