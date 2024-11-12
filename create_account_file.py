92ede6bc66e353b937d733c46a60074bbce3df26
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 16:00:11 -0500

    Update create_account_file.py

--- create_account_file.py
+++ create_account_file.py
@@ -3,17 +3,22 @@ from custom_css_file import get_custom_css_page
 
 # Function to handle the create account page
 def get_create_account_page():
-    get_custom_css_page(alignment="left")
+    #get_custom_css_page(alignment="left")
 
     st.title("Create Your Account")
     st.write("Enter your Bentley University email and password to sign up")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
     
+    # Checkbox for Terms and Conditions
+    agree_terms = st.checkbox("I agree to the Terms and Conditions")
+    
     if st.button("Submit"):
-        # Check if the email ends with @falcon.bentley.edu and contains only one @ symbol
+        # Check if the email ends with @falcon.bentley.edu
         if not email.endswith("@falcon.bentley.edu") or email.count('@') != 1:
             st.error("Please enter a valid Bentley University email address")
+        elif not agree_terms:
+            st.error("You must agree to the Terms and Conditions before submitting.")
         elif email == "admin@falcon.bentley.edu":  # Check if the email is admin
             st.session_state.page = "admin_landing_page"  # Go to admin landing page
             st.success("Account created successfully!")
