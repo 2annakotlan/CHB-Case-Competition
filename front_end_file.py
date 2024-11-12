a20b0839996219635f9959454764465d9ed1cb59
Anna Kotlan <2annakotlan@gmail.com>
2024-11-12 01:55:26 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -5,9 +5,9 @@ from login_signup_file import get_login_signup_page
 
 
 # Example usage with two image URLs
-image_url_1 = "https://example.com/image1.jpg"
-image_url_2 = "https://example.com/image2.jpg"
+image_url_1 = "https://github.com/2annakotlan/CHB-Case-Competition/raw/main/Better Together 1.png"
+image_url_2 = "https://github.com/2annakotlan/CHB-Case-Competition/raw/main/Better Together 2.png"
 get_login_signup_page(image_url_1, image_url_2)
 
-get_login_signup_page("https://github.com/2annakotlan/CHB-Case-Competition/raw/main/homepage_yellow_file.png")
+get_login_signup_page("https://github.com/2annakotlan/CHB-Case-Competition/raw/main/Better Together 2.png")
 #get_friend_swiping_page(friend_swiping_df)
