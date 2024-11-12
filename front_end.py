cd824570cad97a3f141be3798559f2dd976b9012
Anna Kotlan <2annakotlan@gmail.com>
2024-11-11 23:53:14 -0500

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -4,4 +4,4 @@ from friend_swiping import get_friend_swiping_page
 from login_signup import get_login_signup_page
 
 get_login_signup_page("https://github.com/2annakotlan/CHB-Case-Competition/raw/main/homepage_blue.png")
-get_friend_swiping_page(friend_swiping_df)
+#get_friend_swiping_page(friend_swiping_df)
