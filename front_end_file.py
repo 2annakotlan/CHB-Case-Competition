17fc11a293120f366d300d50f5c511163c0573ca
Anna Kotlan <2annakotlan@gmail.com>
2024-11-13 03:37:29 -0500

    Update front_end_file.py

--- front_end_file.py
+++ front_end_file.py
@@ -1,5 +1,4 @@
 # IMPORT PACKAGES AND OBJECTS 
-from data_file import friend_swiping_df
 from friend_swiping_file import get_friend_swiping_page
 from initialize_file import get_initialize_page
 from login_signup_file import get_login_signup_page
