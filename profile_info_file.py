b0fe6aa4baa780fe42bd4b6d7e2b0ea3ec8f8d28
Anna Kotlan <2annakotlan@gmail.com>
2024-11-13 01:40:17 -0500

    Update profile_info_file.py

--- profile_info_file.py
+++ profile_info_file.py
@@ -4,7 +4,7 @@ from create_account_file import get_create_account_page
 from custom_css_file import get_custom_css_page
 
 def get_profile_info_page():
-    get_custom_css_page(alignment="left", button_span="full")
+    get_custom_css_page(alignment="left", button_span="auto")
     
     email_prefix = st.session_state.user_email.split('@')[0]
     formatted_prefix = f"{email_prefix[0].upper()}. {email_prefix[1:].capitalize()}"
