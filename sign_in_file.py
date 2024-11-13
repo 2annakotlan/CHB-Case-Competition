454bd71362e8f50c038a503ee191148822d3c7ba
Anna Kotlan <2annakotlan@gmail.com>
2024-11-13 17:57:44 -0500

    Update sign_in_file.py

--- sign_in_file.py
+++ sign_in_file.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from custom_css_file import get_custom_css_page
+from population_data_file import population_df
 
 # Function to handle the sign-in page
 def get_sign_in_page():
@@ -16,8 +17,11 @@ def get_sign_in_page():
     valid_student_email = valid_email and email != "admin@falcon.bentley.edu"
     valid_admin_email = email == "admin@falcon.bentley.edu"
 
-    # Initialize checkbox for terms acceptance as None
-    agree_terms = None
+    # Check if the email exists in the population data
+    not_existing_email = email.split('@')[0] not in population_df['0_degree'].values
+
+    # Initialize checkbox for terms acceptance
+    agree_terms = st.checkbox("I agree to the terms and conditions.")
     
     enter_clicked = st.button("Enter")
     
@@ -30,7 +34,15 @@ def get_sign_in_page():
         elif valid_admin_email:  # If admin, navigate to admin landing page
             st.session_state.page = "admin_landing_page"
 
-        # 3. Handle Student Role Navigation 
+        # 3. Handle New Accounts
+        elif not_existing_email:
+            st.error("This account does not exist - create a new account.")
+
+        # 4. Handle Student Role Navigation 
         elif valid_student_email:  # If student, navigate to student landing page
             st.session_state.user_email = email
             st.session_state.page = "student_landing_page"
+        
+        # 5. Check for terms acceptance
+        if not agree_terms:
+            st.error("You must agree to the terms and conditions.")
