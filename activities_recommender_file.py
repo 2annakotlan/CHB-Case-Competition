4482694f0e4057410344e8dce1943c83227b1f9c
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 15:35:21 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -1,11 +1,12 @@
 from custom_css_file import get_custom_css_page
 from nth_degree_file import get_full_df
 import streamlit as st
+from population_data_file import population_df
 
 def get_activities_recommender_page(df):
   get_custom_css_page(alignment="center", button_span="full")
-  full_df = get_full_df(df)
-  st.dataframe(full_df.head())
+
+  st.dataframe(population_df.head())
 
   
 
