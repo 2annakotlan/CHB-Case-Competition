baca149e44392cfad66df6f43b3e14768570b573
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 01:52:40 -0500

    Update student_landing_file.py

--- student_landing_file.py
+++ student_landing_file.py
@@ -4,10 +4,11 @@ from custom_css_file import get_custom_css_page
 # Function to handle the student landing page view
 def get_student_landing_page():
     get_custom_css_page(alignment="center", button_span="full")
-    
-    st.title("Student Dashboard")
-    st.write("Welcome, student! This is your personal dashboard.")
-    st.write("Explore resources, courses, and more.")
+
+    email_prefix = st.session_state.user_email.split('@')[0]
+    formatted_prefix = f"{email_prefix[0].upper()}. {email_prefix[1:].capitalize()}"
+    st.title(f"{formatted_prefix}'s Student Dashboard") 
+    st.write("Welcome, f"{formatted_prefix}! This is your personal dashboard.")
     
     image_url_1 = "https://raw.githubusercontent.com/2annakotlan/CHB-Case-Competition/main/swipe.png"
     image_url_2 = "https://raw.githubusercontent.com/2annakotlan/CHB-Case-Competition/main/house.png"
