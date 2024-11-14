e9fb0a41894db162c3deca7c3066440fae534aee
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 15:39:35 -0500

    Update profile_info_file.py

--- profile_info_file.py
+++ profile_info_file.py
@@ -1,3 +1,5 @@
+from custom_css_file import get_custom_css_page
+
 def get_profile_info_page(population_df):
     # Apply custom CSS with specified alignment and button span
     get_custom_css_page(alignment="left", button_span="auto") 
