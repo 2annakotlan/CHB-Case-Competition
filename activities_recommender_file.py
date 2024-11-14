eeb9629be91f370445d17461be6604a775631e9b
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 16:17:14 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -1,5 +1,4 @@
 from custom_css_file import get_custom_css_page
-from nth_degree_file import get_full_df
 import streamlit as st
 #from population_data_file import full_population_df
 import pandas as pd
