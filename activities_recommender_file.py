651f17f7895ea72142dd0e87ba155165d5383495
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 15:42:52 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -3,12 +3,14 @@ from nth_degree_file import get_full_df
 import streamlit as st
 from population_data_file import population_df
 
-def get_activities_recommender_page(df):
-  get_custom_css_page(alignment="center", button_span="full")
+def get_activities_recommender_page():
+    get_custom_css_page(alignment="center", button_span="full")
 
-  st.dataframe(population_df)
+    # Retrieve population_df from session state
+    population_df = st.session_state.get('population_df', pd.DataFrame())  # Default to empty DataFrame if not found
 
-  
+    # Display the updated DataFrame
+    st.dataframe(population_df)
 
 
   
