8e70dad59299d2eab556aa8e0b7a69d990b51c18
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 15:45:28 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -6,12 +6,7 @@ import pandas as pd
 
 def get_activities_recommender_page():
     get_custom_css_page(alignment="center", button_span="full")
-
-    # Retrieve population_df from session state
-    population_df = st.session_state.get('population_df', pd.DataFrame())  # Default to empty DataFrame if not found
-
-    # Display the updated DataFrame
-    st.dataframe(population_df)
+    st.write("HI)
 
 
   
