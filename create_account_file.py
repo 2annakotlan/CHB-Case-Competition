fca1447aaaa2a629673998d3ba5eda1aa546c4da
Anna Kotlan <2annakotlan@gmail.com>
2024-11-13 19:05:56 -0500

    Update create_account_file.py

--- create_account_file.py
+++ create_account_file.py
@@ -12,14 +12,14 @@ def get_create_account_page():
     password = st.text_input("Password", type="password")
     
     # Defining Variables
-    valid_email = email.endswith("@falcon.bentley.edu") 
+    valid_email = email.endswith("@falcon.bentley.edu")
     valid_admin_email = email == "admin@falcon.bentley.edu"
     email_base = email.split('@')[0]  # Get the username part of the email
-    valid_existing_email = email_base in population_df['0_degree'].values
-    valid_new_student_email = email != valid_admin_email
+    valid_student_existing_email = email_base in population_df['0_degree'].values
+    valid_new_student_email = not valid_admin_email and not valid_student_existing_email
 
-    # Initialize checkbox only for students with valid email
-    agree_terms = st.checkbox("I agree to the Terms and Conditions") if valid_student_email else None
+    # Initialize checkbox for terms acceptance only for new students
+    agree_terms = st.checkbox("I agree to the Terms and Conditions") if valid_new_student_email else None
     
     submit_clicked = st.button("Submit")
     
@@ -28,16 +28,16 @@ def get_create_account_page():
         if not valid_email:
             st.error("Please enter a valid Bentley University email address.")
         
-        # 2. Handle admin account creation
+        # 2. Handle admin account navigation
         elif valid_admin_email: 
             st.session_state.page = "admin_landing_page"
 
-        # 3. Handle pre-existing account
-        elif pre_existing_email:
+        # 3. Handle pre-existing student account
+        elif valid_student_existing_email:
             st.error("This account already exists - go back and select sign in.")
         
-        # 4. Handle student account creation and terms agreement
-        elif valid_new_student_email: 
+        # 4. Handle new student account creation and terms agreement
+        elif valid_new_student_email:
             if not agree_terms:
                 st.error("You must agree to the Terms and Conditions to continue.")
             else:
