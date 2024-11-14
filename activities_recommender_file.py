cc5fbd77f1ffde711bffb6c0eb1fc0318d31779d
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 02:30:15 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -1,9 +1,9 @@
 from custom_css_file import get_custom_css_page
-from nth_degree_file import get_nth_degree_page
+from nth_degree_file import second_degree
 
-def get_activities_recommender_page(df_degree):
+def get_activities_recommender_page(second_degree):
   get_custom_css_page(alignment="center", button_span="full")
-  st.dataframe(df_degree.head())
+  st.dataframe(second_degree.head())
 
   
 
