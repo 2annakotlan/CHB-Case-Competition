957c3d64040f5035787d3ad8669e5b4d3de383ba
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 02:02:49 -0500

    Update student_landing_file.py

--- student_landing_file.py
+++ student_landing_file.py
@@ -13,22 +13,18 @@ def get_student_landing_page():
     image_url_1 = "https://raw.githubusercontent.com/2annakotlan/CHB-Case-Competition/main/swipe.png"
     image_url_2 = "https://raw.githubusercontent.com/2annakotlan/CHB-Case-Competition/main/house.png"
     
-    # Define page values for session_state
-    page_1 = "page1"
-    page_2 = "page2"
-
     col1, col2 = st.columns(2)
 
     with col1:
         # Show image and use a button to set session_state.page on click
-        if st.button("Go to Page 1", key="image1_button"):
-            st.session_state.page = "login_signup_page"
+        if st.button("Friend Swiping", key="image1_button"):
+            st.session_state.page = 'friend_swiping_page'
         st.image(image_url_1, width=300)
 
     with col2:
         # Show image and use a button to set session_state.page on click
-        if st.button("Go to Page 2", key="image2_button"):
-            st.session_state.page = "login_signup_page"
+        if st.button("Activities Recommender", key="image2_button"):
+            st.session_state.page = 'friend_swiping_page'
         st.image(image_url_2, width=300)
     
     # Optionally, add a button to go back to the login/signup page
