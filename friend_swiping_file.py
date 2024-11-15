1368d96199c403ba4d4b2689348151a651635fa2
Anna Kotlan <2annakotlan@gmail.com>
2024-11-15 02:59:37 -0500

    Update friend_swiping_file.py

--- friend_swiping_file.py
+++ friend_swiping_file.py
@@ -1,20 +1,14 @@
 # PACKAGES
-#import streamlit as st
-#import pandas as pd
-#from custom_css_file import get_custom_css_page
-#from population_data_file import population_df
+import streamlit as st
+import pandas as pd
+from custom_css_file import get_custom_css_page
 
-# Use the provided image URL for every profile
-#image_url = "https://github.com/2annakotlan/CHB-Case-Competition/blob/main/phone-book.png?raw=true"  # The "?raw=true" is required for GitHub image links
-
-# GET TINDER-LIKE SWIPING VISUAL FUNCTION
+# GET FRIEND SWIPING VISUAL FUNCTION
 def get_friend_swiping_page():
-    # Apply custom CSS for styling
-    #get_custom_css_page()
+   get_custom_css_page()
+   st.title("Friend Swiping")
+   
 
-    # Title of the page
-    #st.title("Friend Swiping")
-    
-    # Back to dashboard button
-    #if st.button('Back to Dashboard'):
-        #st.session_state.page = "student_landing_page"
+    # DONT REMOVE BELOW
+   if st.button('Back to Dashboard'):
+      st.session_state.page = "student_landing_page"
