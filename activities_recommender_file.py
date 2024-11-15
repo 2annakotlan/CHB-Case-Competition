3350e956b52ddae0ff891345d412c92787eccd0a
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 20:33:22 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -9,12 +9,13 @@ def get_activities_recommender_page():
     activities_df = get_activities(full_population_df, 'stest')
     
     # Filter and sort by 2nd-degree connections
+    activities_df = activities_df[activities_df['count_degree_2'] > 0]
     activities_df = activities_df.sort_values(by='count_degree_2', ascending=False)
     
     # Display activities with the highest 2nd-degree connections first
     st.markdown("### 2nd Degree Connections")
     
     for _, row in activities_df.iterrows():
-        total_people = row['total_people']
-        message = f"Join **{row['activities']}** to meet {total_people} new people that share a mutual friend."
+        count = row['count_degree_2']
+        message = f"Join **{row['activities']}** to meet {count} new people that share a mutual friend."
         st.markdown(f"<div style='text-align: left;'>{message}</div>", unsafe_allow_html=True)
