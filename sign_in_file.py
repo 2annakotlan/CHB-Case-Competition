4d62c0503753dc02c6be6bbba45d59acff6ae579
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 20:54:15 -0500

    Update sign_in_file.py

--- sign_in_file.py
+++ sign_in_file.py
@@ -7,16 +7,14 @@ def get_sign_in_page():
     get_custom_css_page(alignment="left", button_span="full")
 
     st.title("Sign In")
-    st.write("Enter your Bentley University email and password to sign in")
+    st.write("Enter admin email and password to sign in")
     
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
+    email = st.text_input("Email", value="admin@falcon.bentley.edu", disabled=True)
+    password = st.text_input("Password", type="password", value="123", disabled=True)
     
     # Defining Variables
-    valid_email = email.endswith("@falcon.bentley.edu")
-    admin_email = email == "admin@falcon.bentley.edu"
-    email_base = email.split('@')[0]  
-    existing_email = email_base in population_df['0_degree'].values
+    email_base = email.split('@')[0] 
+    valid_existing_email = "admin@falcon.bentley.edu"  # Corrected to a string
 
     # Layout the buttons side by side using columns
     col1, col2 = st.columns([1, 1])
@@ -26,24 +24,9 @@ def get_sign_in_page():
     
     with col2:
         next_clicked = st.button("Next")
-
-    if next_clicked:
-        # 1. Validate the Bentley University email
-        if not valid_email:
-            st.error("Please enter a valid Bentley University email address.")
-        
-        # 2. Handle Admin Role Navigation
-        elif admin_email:  # If admin, navigate to admin landing page
-            st.session_state.page = "admin_landing_page"
-
-        # 3. Handle Non-Existing Account
-        elif not existing_email:
-            st.error("This account does not exist - go back and select create account.")
-
-        # 4. Handle Student Role Navigation 
-        elif existing_email:  # If student, navigate to student landing page
-            st.session_state.user_email = email
-            st.session_state.page = "student_landing_page"
     
+    if next_clicked:
+        st.session_state.page = "admin_landing_page"
+
     if back_clicked:
-        st.session_state.page = "login_signup_page"  
+        st.session_state.page = "login_signup_page"
