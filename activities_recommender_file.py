442024214af35bb19fa7645c1fc0934ab84fbe58
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 20:00:52 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -1,14 +1,13 @@
 from custom_css_file import get_custom_css_page
 import streamlit as st
 from population_data_file import full_population_df
-from activities_recommender_alg_file import get_activities
+from activities_recommender_alg_file import get_activities, generate_activities_messages
 import pandas as pd
 
 def get_activities_recommender_page():
     get_custom_css_page(alignment="center", button_span="full")
     activities_df = get_activities(full_population_df, 'stest')
     st.dataframe(activities_df)
-
-
+    generate_activities_messages(activities_df)
 
   
