b6fddc1da2f33d55fc9810f54d812604d6b0dee2
Anna Kotlan <2annakotlan@gmail.com>
2024-11-14 20:31:36 -0500

    Update activities_recommender_file.py

--- activities_recommender_file.py
+++ activities_recommender_file.py
@@ -1,6 +1,8 @@
 from custom_css_file import get_custom_css_page
 import streamlit as st
+from population_data_file import full_population_df
 from activities_recommender_alg_file import get_activities
+import pandas as pd
 
 def get_activities_recommender_page():
     get_custom_css_page(alignment="center", button_span="full")
